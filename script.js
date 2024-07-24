27d4dda8124a5c02ea7b9e2e2f74f7070b2cb6f8
Salomeb22 <salomeberidze76@gmail.com>
2024-07-24 20:19:12 +0400

    hw

---
+++ script.js
@@ -0,0 +1,47 @@
+// varable name
+let firstName = "Salome";
+console.log ("Hello" + " " + firstName);
+
+// What is the difference between "5" and 5?
+// "5" - string 
+// 5 - Number
+
+// variable number
+let age = 12;
+let isBig = age > 9;
+console.log (`age > 9 = ${isBig}`); 
+
+//boolean
+console.log (`10 >= 13 = ${10 >= 13}`);
+
+// comparison operators > , < , >= , <= , == , and !=
+ 
+// upper case
+console.log ("hello".toUpperCase());
+
+// lower case
+console.log ("World".toLowerCase());
+
+// string
+
+// undefined
+
+// it shoud be .toLowerCase() not .toLowercase()
+
+// 11. -
+
+// 12 -
+
+// string number
+
+// number number
+
+//++ or -- before a variable (++x or --x) is prefix. So, it performs the function before the variable is used. ++ or -- after a variable(x++ or x--) is postfix. So, it performs the function after the next use of variable in the statements.
+
+// null
+
+// a == b means a is equal to b 
+
+// a != b means a is not equal tob
+
+// it will be string
